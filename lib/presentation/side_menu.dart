13c8ce2fc2fe4b072ba7d9e02ded961e7db2d885
Alexandra Leonidova <alexandraleonid2002@gmail.com>
2024-11-29 22:27:40 +0500

    sample

---
+++ lib/presentation/side_menu.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Color(0xFFFDFBE8),
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            margin: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
+            child: UserAccountsDrawerHeader (
+              decoration: BoxDecoration(color: Color(0xFF438589)),
+              accountName: const Text('Кися'),
+              accountEmail: const Text("home@dartflutter.ru"),
+              currentAccountPicture: Container(
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image : DecorationImage(
+                        image: AssetImage('assets/images/avataaar.jpg'),
+                        fit: BoxFit.fill,
+                      )
+                  )
+              ),
+            ),
+          ),
+          ListTile(
+              title: const Text("О себе"),
+              leading: const Icon(Icons.account_box),
+              onTap: (){}
+          ),
+          ListTile(
+              title: const Text("Настройки"),
+              leading: const Icon(Icons.settings),
+              onTap: (){}
+          )
+        ],
+      ),
+    );
+  }
+}

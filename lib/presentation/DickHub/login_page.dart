e4ceaf63e988a2b8af6ea317b8b25155bc947ec0
Nikita Ershov <137996541+ersnick@users.noreply.github.com>
2025-01-23 21:24:21 +0500

    страница владельца словаря

--- lib/presentation/DickHub/login_page.dart
+++ lib/presentation/DickHub/login_page.dart
@@ -68,6 +68,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFDFBE8), // Светлый бежевый цвет
       appBar: AppBar(
         title: Text('Вход'),
         backgroundColor: Color(0xFF438589),
